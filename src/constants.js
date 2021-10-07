2208630f60ee5c768bfb43ec67dcc5f864471024
Nick Matthew <nick.matthew414@gmail.com>
2021-10-07 15:16:23 -0400

    added constants file and minor screen size edits

---
+++ src/constants.js
@@ -0,0 +1,17 @@
+
+/*
+All pages
+*/
+export const halfGrid = 5;
+export const fullGrid = 10;
+export const singleColumnWidth = 800;  // pixel with below which we show info in a single column
+
+/* 
+runeDistribution.js
+*/
+
+//pixel width at which we stop showing the entire distribution table
+export const dropTableInfoThreshold = 1030;
+
+// pixel width at which we stop showing the info popups column
+export const tableInfoColumnThreshold = 1200;
